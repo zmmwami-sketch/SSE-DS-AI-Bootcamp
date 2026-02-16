45d286acde911e0db57229ddaec8f2d074ceafc4
zmmwami-sketch <zm.mwami@gmail.com>
2026-02-16 13:05:32 +0300

    Create testchildren.py for student test results
    Add a script to print test results and greet students

---
+++ testchildren.py
@@ -0,0 +1,5 @@
+#adding new fie in the chid branc
+print("Morning students")
+print("Kindly receive your test results")
+name= input("Enter your name")
+remarks= Print(f" congratulations{name} for your good reesluts")
